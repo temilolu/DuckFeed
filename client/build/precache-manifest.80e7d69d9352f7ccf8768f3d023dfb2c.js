fdbb3f3df067f2815112a0847e7c65ae3fa8f1af
temitayo ogunlolu <temitayo094@gmail.com>
2019-08-06 18:15:07 +0100

    updated .gitignore

---
+++ client/build/precache-manifest.80e7d69d9352f7ccf8768f3d023dfb2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ddd34a14364d918adfa5805975c152",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51d9f33633499e725976",
+    "url": "/static/css/main.a4818571.chunk.css"
+  },
+  {
+    "revision": "53bd6a10b93d82979823",
+    "url": "/static/js/2.695f40ed.chunk.js"
+  },
+  {
+    "revision": "51d9f33633499e725976",
+    "url": "/static/js/main.655c669a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

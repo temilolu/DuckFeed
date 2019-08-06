75ad336a780be0892ba339784620b0dd14b127ea
temitayo ogunlolu <temitayo094@gmail.com>
2019-08-06 16:10:41 +0100

    Refactored code base and started implementation for test cases on the client

--- client/src/test/App.test.js
+++ client/src/test/App.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { render, cleanup } from "@testing-library/react";
 import ReactDOM from 'react-dom';
 import App from '../App';
 
-it('renders without crashing', () => {
+afterEach(cleanup);
+
+test('renders without crashing', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(<App />, div);
 	ReactDOM.unmountComponentAtNode(div);
--- client/src/test/Header.test.js
+++ client/src/test/Header.test.js
@@ -1,9 +1,71 @@
-import React from 'react';
-import Header from '../components/Header';
-import renderer from 'react-test-renderer';
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import { Header } from "../components/Header";
 
-describe('Header component', () => {
-	it('matches the snapshot', () => {
-		const tree = renderer.create(<Header />).toJSON();
-	});
+afterEach(cleanup);
+
+describe("Header component", () => {
+  test("matches the snapshot", () => {
+    const tree = render(Header);
+    expect(tree).toMatchInlineSnapshot(`
+      Object {
+        "asFragment": [Function],
+        "baseElement": <body>
+          <div />
+        </body>,
+        "container": <div />,
+        "debug": [Function],
+        "findAllByAltText": [Function],
+        "findAllByDisplayValue": [Function],
+        "findAllByLabelText": [Function],
+        "findAllByPlaceholderText": [Function],
+        "findAllByRole": [Function],
+        "findAllByTestId": [Function],
+        "findAllByText": [Function],
+        "findAllByTitle": [Function],
+        "findByAltText": [Function],
+        "findByDisplayValue": [Function],
+        "findByLabelText": [Function],
+        "findByPlaceholderText": [Function],
+        "findByRole": [Function],
+        "findByTestId": [Function],
+        "findByText": [Function],
+        "findByTitle": [Function],
+        "getAllByAltText": [Function],
+        "getAllByDisplayValue": [Function],
+        "getAllByLabelText": [Function],
+        "getAllByPlaceholderText": [Function],
+        "getAllByRole": [Function],
+        "getAllByTestId": [Function],
+        "getAllByText": [Function],
+        "getAllByTitle": [Function],
+        "getByAltText": [Function],
+        "getByDisplayValue": [Function],
+        "getByLabelText": [Function],
+        "getByPlaceholderText": [Function],
+        "getByRole": [Function],
+        "getByTestId": [Function],
+        "getByText": [Function],
+        "getByTitle": [Function],
+        "queryAllByAltText": [Function],
+        "queryAllByDisplayValue": [Function],
+        "queryAllByLabelText": [Function],
+        "queryAllByPlaceholderText": [Function],
+        "queryAllByRole": [Function],
+        "queryAllByTestId": [Function],
+        "queryAllByText": [Function],
+        "queryAllByTitle": [Function],
+        "queryByAltText": [Function],
+        "queryByDisplayValue": [Function],
+        "queryByLabelText": [Function],
+        "queryByPlaceholderText": [Function],
+        "queryByRole": [Function],
+        "queryByTestId": [Function],
+        "queryByText": [Function],
+        "queryByTitle": [Function],
+        "rerender": [Function],
+        "unmount": [Function],
+      }
+    `);
+  });
 });
